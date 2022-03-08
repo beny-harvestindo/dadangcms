48cbc61109a03307c31739eec7cbe510c224181c
beny.putra <beny.harvestindo@gmail.com>
2022-03-08 10:11:27 +0700

    Fixed by me

---
+++ configuration.php
@@ -0,0 +1,10 @@
+<?php
+$license = 'loremipsum';
+$db_host = 'mysql';
+$db_port = '';
+$db_username = 'whmcs';
+$db_password = '888888';
+$db_name = 'whmcs';
+$cc_encryption_hash = 'f3qJEc3WBzy5PhMWTF6uSoSXJM04HIWtRxHYvFdO9rttDm9SUFB0TfFWrt4mVdBt';
+$templates_compiledir = 'templates_c';
+$mysql_charset = 'utf8';
